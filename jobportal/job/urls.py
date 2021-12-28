ed6c228263d9db796587f1bcc53ddb06ffeb774e
MarjanSadat <marjanmousavi@ymail.com>
2021-12-28 12:37:39 +0330

    add files

--- jobportal/job/urls.py
+++ jobportal/job/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import index, admin_login
+from .views import index, admin_login, user_login, recruiter_login
 
 app_name = 'job'
 urlpatterns = [
     path('', index, name='index'),    
     path('admin_login', admin_login, name='admin_login'),    
+    path('user_login', user_login, name='user_login'),    
+    path('recruiter_login', recruiter_login, name='recruiter_login'),    
 ]
--- jobportal/job/views.py
+++ jobportal/job/views.py
@@ -7,3 +7,9 @@ def index(request):
 
 def admin_login(request):
     return render(request, 'job/admin_login.html')
+
+def user_login(request):
+    return render(request, 'job/user_login.html')
+
+def recruiter_login(request):
+    return render(request, 'job/recruiter_login.html')
